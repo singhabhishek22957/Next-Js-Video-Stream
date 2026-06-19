014e326335039426acb22fd4ab81e1d612fe6172
singhabhishek22957 <singhabhishek22957@gmail.com>
2026-06-19 17:44:50 +0530

    add progressbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/providers/themeProvider";
 import { Toaster } from "sonner";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
     >
       <body suppressHydrationWarning>
         <ThemeProvider>
+          <NextTopLoader color="#FF4D8D" height={3} showSpinner={false} />
           {children}
           <Toaster richColors position="top-right" />
         </ThemeProvider>
